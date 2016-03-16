7d639e5b9f276c67adc1d477b2fcade48e50177e
mendolag <mendola5@hotmail.com>
2016-03-16 13:35:33 +0100

    add schema for group

--- app/models/group.js
+++ app/models/group.js
@@ -1,3 +1,14 @@
 /**
- * Created by josmendola on 16/03/16.
+ * Created by josmendola on 27/01/16.
  */
+var mongoose = require('mongoose');
+var Schema=mongoose.Schema,
+    objectId=Schema.ObjectId;
+
+var groupSchema = mongoose.Schema({
+    name:String,
+    users:[{type:objectId, ref:'User'}]
+});
+
+
+module.exports=mongoose.model('Group', groupSchema);
